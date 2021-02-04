034e2c648e12dd247dec530653f51dd53284e8ed
morgan-jaouni <morgan314@gmail.com>
2021-02-03 23:45:46 -0600

    Server Endpoints

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -4,9 +4,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const jobs = require('./models/Job');
+const jobRoutes = express.Router();
 const PORT = 4000;
 
 app.use(cors());
+app.use('/jobs', jobRoutes);
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/job-list', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,6 +18,20 @@ const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
+
+
+jobRoutes.route('/').get(function (req,res) {
+    jobs.find(function(err, jobs) {
+
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(jobs);
+        }
+
+    });
+});
+
 
 app.listen(PORT, function() {
     console.log("Server running on Port: " + PORT );
